f189ff21ff291b9eaaf8f8d007a6fb0a5accd427
SireKaze <114274610+SireKaze@users.noreply.github.com>
2024-03-25 11:30:28 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      d1: '#00A2C6',
+      d2: '#2bd8ff',
+    },
+  },
+  plugins: [],
+}
+
